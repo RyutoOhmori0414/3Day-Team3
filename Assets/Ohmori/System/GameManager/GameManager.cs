a51098aa665b3a88d922312491437c8045d109b3
大森 龍人 <106291438+RyutoOhmori0414@users.noreply.github.com>
2025-02-25 14:23:48 +0900

    add: 敵の数とスコア管理を追加しました。

--- Assets/Ohmori/System/GameManager/GameManager.cs
+++ Assets/Ohmori/System/GameManager/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,8 +9,55 @@ public sealed class GameManager : SingletonMonoBehaviour<GameManager>
     private float _readySeconds = 3f;
 
     public float CurrentReadySeconds { get; private set; }
-
     public float CurrentGameElapsedSeconds { get; private set; }
+
+    private int _defeatEnemyCount = 0;
+    private int _score = 0;
+
+    /// <summary>現在の倒したEnemy</summary>
+    public int DefeatEnemyCount => _defeatEnemyCount;
+    /// <summary>現在のスコア</summary>
+    public int Score => _score;
+
+    /// <summary>エネミーが増加した際に呼ばれて、引数として現在の倒した敵の数が渡されます</summary>
+    public event Action<int> OnEnemyDefeated;
+    /// <summary>スコアが増えた際に呼ばれます、引数として現在のスコアが渡されます</summary>
+    public event Action<int> OnScoreChanged;
+
+    protected override void OnThisDestroy()
+    {
+        base.OnThisDestroy();
+
+        OnEnemyDefeated = null;
+        OnScoreChanged = null;
+    }
+
+    /// <summary>倒したEnemyが一体増えます</summary>
+    public void DefeatEnemy()
+    {
+        _defeatEnemyCount++;
+
+        OnEnemyDefeated?.Invoke(_defeatEnemyCount);
+    }
+
+    /// <summary>スコアを追加します</summary>
+    /// <param name="addScore">追加するスコア</param>
+    public void AddScore(int addScore)
+    {
+        _score += addScore;
+
+        OnScoreChanged?.Invoke(_score);
+    }
+
+    /// <summary>カウントと登録した関数をリセットします。</summary>
+    public void ResetParams()
+    {
+        _defeatEnemyCount = 0;
+        _score = 0;
+
+        OnEnemyDefeated = null;
+        OnScoreChanged = null;
+    }
 
     private Coroutine _currentGame = null;
 
@@ -107,6 +155,7 @@ public sealed class GameManager : SingletonMonoBehaviour<GameManager>
     {
         if (_currentGame is not null) return;
 
+        ResetParams();
         _currentGame = StartCoroutine(GamePlay());
     }
 
