a24c7104e7da6b699868beff7486341a78232831
yuzu1012 <yuzusonyuzuha@gmail.com>
2025-02-25 10:59:35 +0900

    シーン遷移処理

---
+++ Assets/yamamoto/TransiitionButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransiitionButton : MonoBehaviour
+{
+    public void Title()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
