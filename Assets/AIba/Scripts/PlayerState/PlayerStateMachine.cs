00029697ba0417ff6a73f16465c8a431d455b5a8
Aiba_0325 <bantan.ab325@gmail.com>
2025-02-25 12:27:18 +0900

    Playerの基盤

---
+++ Assets/AIba/Scripts/PlayerState/PlayerStateMachine.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[System.Serializable]
+public class PlayerStateMachine : StateMachine
+{
+
+    #region State
+    [SerializeField]
+    private IdleState _stateIdle = default;
+    [SerializeField]
+    private WalkState _stateWalk = default;
+
+
+
+    private PlayerControl _playerController = null;
+
+    public PlayerControl PlayerControl => _playerController;
+
+    public IdleState StateIdle => _stateIdle;
+    public WalkState StateWalk => _stateWalk;
+
+    #endregion
+    [SerializeField]
+    //private GroundCheck _groundCheck;
+    //public GroundCheck GroundCheck => _groundCheck;
+
+    public void Init(PlayerControl playerController)
+    {
+        _playerController = playerController;
+        Initialize(_stateIdle);
+    }
+
+    protected override void StateInit()
+    {
+        _stateIdle.Init(this);
+        _stateWalk.Init(this);
+    }
+
+
+
+}
