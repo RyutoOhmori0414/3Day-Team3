0ee80945df261f5abee0e22d3b74cf5875f5c2d1
yuzu1012 <yuzusonyuzuha@gmail.com>
2025-02-25 16:15:23 +0900

    Resultのテキストアニメーションを変更

--- Assets/yamamoto/Script/BackGroundAnimation.cs
+++ Assets/yamamoto/Script/BackGroundAnimation.cs
@@ -7,20 +7,42 @@ public class BackGroundAnimation : MonoBehaviour
     [Header("リザルトテキスト")]
     public TextMeshProUGUI tex_EnemyCount;
     public TextMeshProUGUI tex_SurviveTime;
+    public TextMeshProUGUI tex_Count;
+    public TextMeshProUGUI tex_Time;
+
+    [Header("テキストのアニメーション秒数")]
+    public float time;
+
+    private int minutes;
+    private int seconds;
 
     void Start()
     {
-        tex_EnemyCount.text = GameManager.I.DefeatEnemyCount.ToString() + " " + "tai";
-        tex_SurviveTime.text = GameManager.I.Score.ToString() + " " + "jikann";
+        tex_EnemyCount.text = GameManager.I.DefeatEnemyCount.ToString();
+        
+        //分秒
+        minutes = (int)GameManager.I.CurrentReadySeconds / 60;
+        seconds = (int)GameManager.I.CurrentReadySeconds % 60;
+        tex_SurviveTime.text = minutes + " " +  ":" +  " " + seconds;
 
         var tex_EnCo = tex_EnemyCount.GetComponent<RectTransform>();
         tex_EnCo.anchoredPosition = new Vector2(0, -600);
-        tex_EnCo.transform.DOMoveY(650f, 5f)
-            .SetEase(Ease.OutBack);
+        tex_EnCo.transform.DOMoveY(650f, time)
+            .SetEase(Ease.OutBounce);
         
         var tex_SuTi = tex_SurviveTime.GetComponent<RectTransform>();
         tex_SuTi.anchoredPosition = new Vector2(0, -715);
-        tex_SuTi.transform.DOMoveY(550f, 5f)
-            .SetEase(Ease.OutBack);
+        tex_SuTi.transform.DOMoveY(550f, time)
+            .SetEase(Ease.OutBounce);
+
+        var tex_Co = tex_Count.GetComponent<RectTransform>();
+        tex_Co.anchoredPosition = new Vector2(500, -600);
+        tex_Co.transform.DOMoveY(650f, time)
+            .SetEase(Ease.OutBounce);
+
+        var tex_Ti = tex_Time.GetComponent<RectTransform>();
+        tex_Ti.anchoredPosition = new Vector2(500, -715);
+        tex_Ti.transform.DOMoveY(550f, time)
+            .SetEase(Ease.OutBounce);
     }
 }
