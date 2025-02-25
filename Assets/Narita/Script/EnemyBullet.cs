0aa0a7ae1ac983ccb75b2fb272a42d2dbdde0df8
NaritaShosei <onigirikun7171@gmail.com>
2025-02-25 12:05:43 +0900

    [add]敵の弾

---
+++ Assets/Narita/Script/EnemyBullet.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class EnemyBullet : MonoBehaviour
+{
+    [SerializeField] int _damage;
+    [SerializeField] float _speed = 2;
+    GameObject _player;
+    Vector3 _dir;
+    void Start()
+    {
+        _dir = _player.transform.position - transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position += _dir.normalized * _speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out DebugHP hP))
+        {
+            hP.AddDamage(_damage);
+        }
+        Destroy(gameObject);
+    }
+    public void AddTarget(GameObject player)
+    {
+        _player = player;
+    }
+}
