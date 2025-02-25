2f8d2ae81247c0fcd2d108c8104585cdb83a71a3
Aiba_0325 <bantan.ab325@gmail.com>
2025-02-25 16:18:22 +0900

    プレイヤーの基礎

---
+++ Assets/AIba/Scripts/PlayerBullet.cs
@@ -0,0 +1,45 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    [Header("速度")]
+    [SerializeField] private float _speed = 7;
+
+    [Header("威力")]
+    [SerializeField] private int _attackPower = 7;
+
+    [Header("消えるまでの時間")]
+    [SerializeField] private float _lifeTime = 10f;
+
+
+    private float _countDestroyTime = 0;
+
+    public void Init(Vector3 dir)
+    {
+        gameObject.GetComponent<Rigidbody>().linearVelocity = dir.normalized * _speed;
+    }
+
+
+    void Update()
+    {
+        _countDestroyTime += Time.deltaTime;
+
+        if (_countDestroyTime > _lifeTime)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            other?.GetComponent<IDamageble>()?.AddDamage(_attackPower);
+        }
+
+        Destroy(gameObject);
+    }
+
+}
--- Assets/AIba/Scripts/PlayerControl.cs
+++ Assets/AIba/Scripts/PlayerControl.cs
@@ -1,10 +1,20 @@
 ﻿using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
+public class PlayerControl : MonoBehaviour,IDamageble
 {
     [Header("移動設定")]
     [SerializeField] private PlayerMove _move;
+
+    [Header("攻撃設定")]
+    [SerializeField] private PlayerAttack _attack;
+
+    [Header("体力")]
+    [SerializeField] private PlayerHp _hp;
+
+    [Header("カメラ設定")]
+    [SerializeField] private PlayerCamera _cameraSetting;
+
 
     [Header("PlayerのRigidbody")]
     [SerializeField] private Rigidbody _rb;
@@ -12,6 +22,9 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] private PlayerStateMachine _stateMachine = default;
 
     public PlayerMove Move => _move;
+    public PlayerCamera CameraSetting => _cameraSetting;
+    public PlayerAttack Attack => _attack;
+    public PlayerHp Hp => _hp;
 
     public Rigidbody Rb => _rb;
     public InputManager InputM => _input;
@@ -20,17 +33,23 @@ public class PlayerControl : MonoBehaviour
     {
         _stateMachine.Init(this);
         _move.Init(this);
+        _cameraSetting.Init(this);
+        _attack.Init(this);
+        _hp.Init(this);
     }
 
 
     void Update()
     {
         _stateMachine.Update();
+        _attack.Charge();
     }
 
     private void FixedUpdate()
     {
         _stateMachine.FixedUpdate();
+
+        // Debug.Log(_rb.linearVelocity);
     }
 
     private void LateUpdate()
@@ -38,4 +57,8 @@ public class PlayerControl : MonoBehaviour
         _stateMachine.LateUpdate();
     }
 
+    public void AddDamage(int damagePoint)
+    {
+        _hp.Damage(damagePoint);
+    }
 }
