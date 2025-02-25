b4058bd56342a7f4f2ca890be9a5f02df3c950cc
大森 龍人 <106291438+RyutoOhmori0414@users.noreply.github.com>
2025-02-25 17:10:16 +0900

    add: SceneManagerの追加

---
+++ Assets/Ohmori/System/SceneManager/DissolveSceneChanger.cs
@@ -0,0 +1,54 @@
+﻿using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+
+public sealed class DissolveSceneChanger : MonoBehaviour
+{
+    [SerializeField]
+    private Shader _dissolveShader;
+    [SerializeField]
+    private Texture2D _dissolveTex;
+    [SerializeField]
+    private float _dissolveDuration;
+
+    private static readonly int _mainTexPropertyID = Shader.PropertyToID("_MainTex");
+    private static readonly int _dissolveAmountPropertyID = Shader.PropertyToID("_DissolveAmount");
+    private static readonly int _dissolveTexPropertyID = Shader.PropertyToID("_DissolveTex");
+
+    private Texture2D _screenShotTex;
+    private Material _material;
+
+
+    private void Awake()
+    {
+        var camera = Camera.main;
+        _screenShotTex = new Texture2D(camera.pixelWidth, camera.pixelHeight, TextureFormat.RGB24, false);
+        _material = new Material(_dissolveShader);
+    }
+
+    public void TakeScreenShot()
+    {
+        _screenShotTex.ReadPixels(new Rect(0, 0, _screenShotTex.width, _screenShotTex.height), 0, 0);
+        _screenShotTex.Apply();
+    }
+
+    public void SetTexture()
+    {
+        _material.SetTexture(_mainTexPropertyID, _screenShotTex);
+        _material.SetTexture(_dissolveTexPropertyID, _dissolveTex);
+    }
+
+    public IEnumerator DissolveAnimation()
+    {
+        _material.SetFloat(_dissolveAmountPropertyID, 0F);
+
+        yield return DOTween.To(
+            () => 0F,
+            value => _material.SetFloat(_dissolveAmountPropertyID, value),
+            1F,
+            _dissolveDuration
+            ).SetLink(gameObject);
+
+        _material.SetFloat(_dissolveAmountPropertyID, 1F);
+    }
+}
---
+++ Assets/Ohmori/System/SceneManager/SingletonSceneManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public sealed class SingletonSceneManager : SingletonMonoBehaviour<SingletonSceneManager>
+{
+    [SerializeField]
+    private DissolveSceneChanger _sceneChanger;
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
