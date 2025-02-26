5a763a970103926448a81cc510ab0e2120e7af3d
yuzu1012 <yuzusonyuzuha@gmail.com>
2025-02-26 12:46:37 +0900

    no message

--- Assets/yamamoto/Script/BackGroundAnimation.cs
+++ Assets/yamamoto/Script/BackGroundAnimation.cs
@@ -10,8 +10,7 @@ public class BackGroundAnimation : MonoBehaviour
     public TextMeshProUGUI tex_Count;
     public TextMeshProUGUI tex_Time;
     public TextMeshProUGUI tex_Score;
-    public TextMeshProUGUI tex_ScoreName;
-
+    
     [Header("テキストのアニメーション秒数")]
     public float time;
 
@@ -23,29 +22,29 @@ public class BackGroundAnimation : MonoBehaviour
         tex_EnemyCount.text = GameManager.I.DefeatEnemyCount.ToString();
         
         //分秒
-        minutes = (int)GameManager.I.CurrentReadySeconds / 60;
-        seconds = (int)GameManager.I.CurrentReadySeconds % 60;
-        tex_SurviveTime.text = minutes + " " +  ":" +  " " + seconds;
+        minutes = (int)GameManager.I.CurrentGameElapsedSeconds / 60;
+        seconds = (int)GameManager.I.CurrentGameElapsedSeconds % 60;
+        tex_SurviveTime.text = minutes.ToString("00") + ":" + seconds.ToString("00");
 
         tex_Score.text = GameManager.I.Score.ToString();
 
         var tex_EnCo = tex_EnemyCount.GetComponent<RectTransform>();
-        tex_EnCo.anchoredPosition = new Vector2(0, -600);
+        tex_EnCo.anchoredPosition = new Vector2(0, -700);
         tex_EnCo.transform.DOMoveY(650f, time)
             .SetEase(Ease.InOutQuad);
         
         var tex_SuTi = tex_SurviveTime.GetComponent<RectTransform>();
-        tex_SuTi.anchoredPosition = new Vector2(0, -715);
+        tex_SuTi.anchoredPosition = new Vector2(0, -815);
         tex_SuTi.transform.DOMoveY(500f, time)
             .SetEase(Ease.InOutQuad);
 
         var tex_Co = tex_Count.GetComponent<RectTransform>();
-        tex_Co.anchoredPosition = new Vector2(500, -600);
+        tex_Co.anchoredPosition = new Vector2(500, -700);
         tex_Co.transform.DOMoveY(650f, time)
             .SetEase(Ease.InOutQuad);
 
         var tex_Ti = tex_Time.GetComponent<RectTransform>();
-        tex_Ti.anchoredPosition = new Vector2(500, -715);
+        tex_Ti.anchoredPosition = new Vector2(500, -815);
         tex_Ti.transform.DOMoveY(500f, time)
             .SetEase(Ease.InOutQuad);
     }
---
+++ Assets/yamamoto/Script/TitleAnim.cs
@@ -0,0 +1,46 @@
+﻿using DG.Tweening;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class TitleAnim : MonoBehaviour
+{
+    public Title title; //ディゾルブ処理のスクリプト
+
+    [Header("アニメーション待機時間")]
+    public float waitTime;
+
+    [Header("テキストアニメーション時間")]
+    public float animTime;
+
+    [Header("表示するテキスト")]
+    public TextMeshProUGUI startText;
+
+    [Header("UIオブジェクト")]
+    public GameObject uiObj;
+
+    private void Start()
+    {
+        startText.enabled = false; //非表示
+    }
+
+    public void GameStart()
+    {
+        uiObj.SetActive(false); //UIのオブジェクトを非表示
+        startText.enabled = true; //表示
+
+        var stText = startText.GetComponent<RectTransform>();
+        stText.anchoredPosition = new Vector2(0, 0);
+        stText.transform.DOScale(new Vector2(2, 2), animTime)
+            .SetEase(Ease.OutElastic);
+        
+        StartCoroutine(UIAnimWait()); //待機
+    }
+
+    IEnumerator UIAnimWait()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        title.GoNextScene();
+    }
+}
